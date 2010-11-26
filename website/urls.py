c0b507d0686418b48ae4429a46e7ff6884e70293
andrew <andrew@106e5988-e85e-4e38-ae25-9a4133eeb102>
2010-11-26 10:14:21 +0000

    Fix some URLs
    git-svn-id: http://core.fort/svn/trunk@67 106e5988-e85e-4e38-ae25-9a4133eeb102

--- website/urls.py
+++ website/urls.py
@@ -9,7 +9,7 @@ urlpatterns = patterns('',
     (r'^$', 'homepage.views.homepage'),
     url(r'^page/(?:(?P<timestamp>-?\d{2}:\d{2}:\d{2}:\d{2})/)?$', PageView.as_view(), name="view_page"),
     url(r'^(?P<start>-?[0-9]+)/$', RangeView.as_view(), name="view_range"),
-    (r'^phases/$', PhasesView.as_view()),
+    url(r'^phases/$', PhasesView.as_view(), name="phases"),
 )
 
 if settings.DEBUG: # pragma: no cover
