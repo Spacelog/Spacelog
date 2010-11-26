d1c5b64c82083fa49d6618cdce11552db300af46
david <david@106e5988-e85e-4e38-ae25-9a4133eeb102>
2010-11-26 18:59:09 +0000

    Add optipng to the extract. Save 10% of the space and take 600% of the time
    git-svn-id: http://core.fort/svn/trunk@127 106e5988-e85e-4e38-ae25-9a4133eeb102

--- tools/pdf_to_images.py
+++ tools/pdf_to_images.py
@@ -6,7 +6,7 @@ import sys
 if len(sys.argv) < 2:
     print >>sys.stderr, "Usage: python pdf_to_images.py [pdf file]"
     print >>sys.stderr
-    print >>sys.stderr, "Converts a PDF into a directory full of PNGs. Requires ImageMagick."
+    print >>sys.stderr, "Converts a PDF into a directory full of PNGs. Requires ImageMagick. & optipng"
     sys.exit(1)
 
 pdf_file = sys.argv[1]
@@ -17,14 +17,29 @@ page = 1
 
 while True:
     print "Converting page %s..." % page
+    png_file = os.path.join(output_dir, '%s.png' % page)
+
     exit_code = subprocess.call([
         'convert', 
         '-density', '300', 
         u'%s[%s]' % (pdf_file, page-1), # zero indexed pages
-        os.path.join(output_dir, '%s.png' % page),
+	png_file,
     ])
     if exit_code != 0:
         break
+
+    # This usually takes many times longer than the extract
+    print "Optimising %s..." % png_file
+    subprocess.call([
+	'optipng',
+	'-q',
+	'-o7',
+	png_file,
+    ])
+    if exit_code != 0:
+	print >>sys.stderr, "optipng failed"
+	sys.exit(1)
+
     page += 1
 
 
