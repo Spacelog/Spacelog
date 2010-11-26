6fc9242758b8a4a7e4db060b00e8f727bc872e8e
andrew <andrew@106e5988-e85e-4e38-ae25-9a4133eeb102>
2010-11-26 15:05:30 +0000

    Make search page slightly nicer.
    git-svn-id: http://core.fort/svn/trunk@90 106e5988-e85e-4e38-ae25-9a4133eeb102

--- website/apps/search/views.py
+++ website/apps/search/views.py
@@ -1,21 +1,26 @@
 import os
 from django.views.generic import TemplateView
 from django.conf import settings
+from django.utils.safestring import mark_safe
 import xappy
+import redis
+from backend.api import LogLine
 
 class SearchView(TemplateView):
 
     template_name = 'search/results.html'
 
     def get_context_data(self):
+        # Get the query text
         q = self.request.GET.get('q', '')
+        # Get the offset value
         try:
             offset = int(
                 self.request.GET.get('offset', '0')
             )
         except ValueError:
             offset = 0
-        
+        # Get the results from Xapian
         db = xappy.SearchConnection(
             os.path.join(
                 settings.SITE_ROOT,
@@ -31,11 +36,20 @@ class SearchView(TemplateView):
             startrank=offset,
             endrank=offset+20, # FIXME: magic number
         )
+        # Go through the results, building a list of LogLine objects
+        redis_conn = redis.Redis()
+        log_lines = []
+        for result in results:
+            transcript_name, timestamp = result.id.split(":", 1)
+            log_line = LogLine(redis_conn, transcript_name, int(timestamp))
+            log_line.speaker = result.data['speaker'][0]
+            log_line.summary = mark_safe(result.summarise("text", maxlen=100, ellipsis='&hellip;', hl=('<em>', '</em>')))
+            log_lines.append(log_line)
 
         # FIXME: db.close()
         
         return {
-            'results': results,
+            'log_lines': log_lines,
             'q': q,
-            'offset': 0,
+            'offset': offset,
         }
