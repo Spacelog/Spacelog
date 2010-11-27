c7b07cf59d4bcc1462fb898557d9ce5854417549
ben <ben@106e5988-e85e-4e38-ae25-9a4133eeb102>
2010-11-27 22:53:20 +0000

    Start of magic javascript loading
    git-svn-id: http://core.fort/svn/trunk@266 106e5988-e85e-4e38-ae25-9a4133eeb102

--- website/static/js/global.js
+++ website/static/js/global.js
@@ -1,99 +1,172 @@
 (function() {
-    var Artemis = {};
+    var Artemis = {
+        // Parses a mission timestamp from a URL and converts it to a number of 
+        // seconds
+        parseMissionTime: function(mission_time) {
+            t = _.map(mission_time.split(':'), function(i) {
+                return parseInt(i, 10);
+            });
+            return t[3] + t[2]*60 + t[1]*3600 + t[0]*86400;
+        }
+    };
 
     // Backbone.History, but using pushState instead of hashes
     Artemis.History = function() {
         this.handlers = [];
         _.bindAll(this, 'loadUrl');
+        this.path = this.getPath();
     };
 
-    var slashRegex = /^\/+/;
-
     _.extend(Artemis.History.prototype, {
-        _stripSlash: function(s) {
-            return s.replace(slashRegex, '');
-        },
-
         getPath: function(loc) {
-            return this._stripSlash((loc || window.location).pathname);
+            return (loc || window.location).pathname;
         },
 
         start: function() {
             var self = this;
             $(window).bind('popstate', function() {
-                self.loadUrl(self.getPath());
+                var path = self.getPath();
+                if (path != this.path) {
+                    var callback = self.routeUrl(path);
+                    if (callback) {
+                        callback(path);
+                    }
+                }
             });
             $('a').live('click', function(e) {
                 // Only absolute links work
-                href = $(this).attr('href');
-                if (href && href[0] == '/') {
-                    href = self._stripSlash(href);
-                    self.loadUrl(href);
-                    e.preventDefault();
-                    return false;
+                path = $(this).attr('href');
+                if (path && path[0] == '/') {
+                    var callback = self.routeUrl(path);
+                    if (callback) {
+                        self.saveLocation(path);
+                        callback(path);
+                        e.preventDefault();
+                        return false;
+                    }
+                    else {
+                        self.navigateToPath(path);
+                    }
                 }
             });
-            self.loadUrl(path);
         },
 
         route: Backbone.History.prototype.route,
-
-        loadUrl: function(path) {
-            if (this.routeUrl(path)) {
-                if (path != this.getPath()) {
-                    this.saveLocation(path);
-                }
-            }
-            else if (path != this.getPath()) {
-                window.location = '/'+path;
-            }
-        },
 
         // Attempt to load the current URL fragment. If a route succeeds with a
         // match, returns `true`. If no defined routes matches the fragment,
         // returns `false`.
         routeUrl: function(path) {
-            var matched = _.any(this.handlers, function(handler) {
-                if (handler.route.test(path)) {
-                    handler.callback(path);
-                    return true;
+            for (var i = 0; i < this.handlers.length; i++) {
+                if (this.handlers[i].route.test(path)) {
+                    return this.handlers[i].callback;
                 }
-            });
-            return matched;
+            }
         },
         
         saveLocation: function(path) {
-            history.pushState(null, document.title, '/'+path);
+            this.path = path;
+            history.pushState(null, document.title, path);
+        },
+
+        navigateToPath: function(path) {
+            if (path === undefined) {
+                path = this.getPath();
+            }
+            window.location = path;
         }
     });
 
     Artemis.LogLine = Backbone.Model.extend({
         
+    }, {
+        fromElement: function(e) {
+            return new Artemis.LogLine({
+                'id': parseInt(e.id.split('-')[2], 10)
+            });
+        }
     });
 
     Artemis.LogLineCollection = Backbone.Collection.extend({
+        model: Artemis.LogLine,
+        initialize: function(models, options) {
+            _.bindAll(this);
+        },
 
+        updateFromElement: function(source) {
+            if (!source) {
+                source = $('#transcript');
+            }
+            
+            this.add(_.map(source.children('div'), _.bind(function(e) {
+                var logLine = new Artemis.LogLine.fromElement(e);
+                return logLine;
+            }, this)));
+        }
     });
 
+    var tspatt = '-?\\d{2}:\\d{2}:\\d{2}:\\d{2}';
+
     Artemis.Controller = Backbone.Controller.extend({
-        routes: {
-            //'': 'homepage',
-            //'phases/': 'phases'
+        initialize: function(options) {
+            this.route('/page/', 'page', this.page);
+            this.route('/page/('+tspatt+')/', 'page', this.page);
         },
 
-        homepage: function() {
-        },
+        page: function(start, end) {
+            if (!$('#content').hasClass('transcript')) {
+                Backbone.history.navigateToPath();
+                return;
+            }
+            if (!this.logLineCollection) {
+                this.logLineCollection = new Artemis.LogLineCollection();
+                this.logLineCollection.updateFromElement();
+            }
+            else {
+                $.getJSON(Backbone.history.getPath(), _.bind(function(data) { 
+                    $('title').text(data.title);
+                    // If there is a crest, load the entire page
+                    if ($(data.crest).children().length) {
+                        $('#content').children().html(data.content);
+                        $('#crest').replaceWith(data.crest);
+                    }
+                    else {
+                        content = $(data.content);
+                        var transcript = content.filter('#transcript');
+                        this.logLineCollection.updateFromElement(transcript);
+                        if (parseInt(transcript.children('div').attr('id').split('-')[2], 10) < this.logLineCollection.first().id) {
+                            $('#transcript').prepend(transcript.children());
+                        }
+                        else {
+                            $('#transcript').append(transcript.children());
+                        }
+                        if ($('#load-previous')) {
+                            $('#load-previous').replaceWith(content.find('#load-previous'));
+                        }
+                        if ($('#load-more')) {
+                            $('#load-more').replaceWith(content.find('#load-more'));
+                        }
+                    }
 
-        phases: function() {
+                }, this));
+            }
+        },
         
+
+        _routeToRegExp: function(route) {
+            return new RegExp('^' + route + '$');
         }
     });
 
     $(function() {
         Backbone.history = new Artemis.History();
-        new Artemis.Controller();
+        Artemis.controller = new Artemis.Controller();
         Backbone.history.start();
     });
 
     window.Artemis = Artemis;
 })();
+
+
+
+
