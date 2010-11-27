4058a738b5d7431c062760983ae2181e082d8868
andrew <andrew@106e5988-e85e-4e38-ae25-9a4133eeb102>
2010-11-27 14:08:32 +0000

    Fix search to lock to the right mission.
    git-svn-id: http://core.fort/svn/trunk@193 106e5988-e85e-4e38-ae25-9a4133eeb102

--- website/apps/search/views.py
+++ website/apps/search/views.py
@@ -42,7 +42,7 @@ class SearchView(TemplateView):
         query=db.query_filter(
             query,
             # FIXME: mission-specific filter!
-            xapian.Query(db._field_mappings.get_prefix("mission") + "a13"),
+            xapian.Query(db._field_mappings.get_prefix("mission") + self.request.mission.name),
         )
         results = db.search(
             query=query,
