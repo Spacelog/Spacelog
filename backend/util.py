183c75dee9f04fe16b3c40d5d089d3c63a847fad
james <james@106e5988-e85e-4e38-ae25-9a4133eeb102>
2010-11-28 13:32:50 +0000

    Cope with negative timestamps.
    git-svn-id: http://core.fort/svn/trunk@311 106e5988-e85e-4e38-ae25-9a4133eeb102

--- backend/util.py
+++ backend/util.py
@@ -8,6 +8,11 @@ def seconds_to_timestamp(seconds):
     )
 
 def timestamp_to_seconds(timestamp):
+    if timestamp[0]=='-':
+        timestamp = timestamp[1:]
+        mult = -1
+    else:
+        mult = 1
     parts = map(int, timestamp.split(":", 3))
-    return (parts[0] * 86400) + (parts[1] * 3600) + (parts[2] * 60) + parts[3]
+    return mult * ((parts[0] * 86400) + (parts[1] * 3600) + (parts[2] * 60) + parts[3])
 
