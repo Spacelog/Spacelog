105b36cda03ea3fd209a8669560360162a2fb2f9
steve <steve@106e5988-e85e-4e38-ae25-9a4133eeb102>
2010-11-28 17:43:11 +0000

    Epic global home updates; add coming soon, add footer, etc.
    git-svn-id: http://core.fort/svn/trunk@394 106e5988-e85e-4e38-ae25-9a4133eeb102

--- global/apps/homepage/views.py
+++ global/apps/homepage/views.py
@@ -4,9 +4,30 @@ import redis
 
 def homepage(request):
     missions = list(Mission.Query(redis.Redis()))
+    missions_coming_soon = [
+        {
+            'name': 'g7',
+            'coming_soon': True,
+            'title': 'Gemini 7',
+            'description': 'One half of the first orbital rendezvous.'
+        },
+        {
+            'name': 'a8',
+            'coming_soon': True,
+            'title': 'Apollo 8',
+            'description': 'The first human space flight to leave Earth orbit.'
+        },
+        {
+            'name': 'a11',
+            'coming_soon': True,
+            'title': 'Apollo 11',
+            'description': 'The first manned mission to land on the moon.'
+        }
+    ]
     return render_to_response(
         'homepage/homepage.html',
         {
-            'missions': missions
+            'missions': missions,
+            'missions_coming_soon': missions_coming_soon
         },
     )
