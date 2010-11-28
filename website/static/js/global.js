6ff2fe217e73f82b90cba45526ba8f8027cd6983
ben <ben@106e5988-e85e-4e38-ae25-9a4133eeb102>
2010-11-28 14:22:03 +0000

    Scary awful broken JavaScript
    git-svn-id: http://core.fort/svn/trunk@319 106e5988-e85e-4e38-ae25-9a4133eeb102

--- website/static/js/global.js
+++ website/static/js/global.js
@@ -19,23 +19,25 @@
 
     _.extend(Artemis.History.prototype, {
         getPath: function(loc) {
-            return (loc || window.location).pathname;
+            return this._cleanPath((loc || window.location).pathname);
+        },
+
+        _cleanPath: function(path) {
+            return path.replace(/#.+?$/, '').replace(/\?.+?$/, '');
         },
 
         start: function() {
             var self = this;
             $(window).bind('popstate', function() {
                 var path = self.getPath();
-                if (path != this.path) {
-                    var callback = self.routeUrl(path);
-                    if (callback) {
-                        callback(path);
-                    }
+                var callback = self.routeUrl(path);
+                if (callback) {
+                    callback(path);
                 }
             });
             $('a').live('click', function(e) {
                 // Only absolute links work
-                path = $(this).attr('href');
+                path = self._cleanPath($(this).attr('href'));
                 if (path && path[0] == '/') {
                     var callback = self.routeUrl(path);
                     if (callback) {
@@ -81,6 +83,7 @@
         
     }, {
         fromElement: function(e) {
+            console.debug(e);
             return new Artemis.LogLine({
                 'id': parseInt(e.id.split('-')[2], 10)
             });
@@ -91,15 +94,47 @@
         model: Artemis.LogLine,
         initialize: function(models, options) {
             _.bindAll(this);
+            this.previousId = null;
+            this.nextId = null;
         },
 
-        updateFromElement: function(source) {
-            if (!source) {
-                source = $('#transcript');
+        updateFromContent: function(sourceContent) {
+            var destContent, destTranscript;
+            if (!sourceContent) {
+                sourceContent = $('#content').children().children();
+                var sourceTranscript = sourceContent.filter('#transcript');
+            }
+            else {
+                var sourceTranscript = sourceContent.filter('#transcript');
+                destContent = $('#content');
+                destTranscript = destContent.find('#transcript');
             }
             
-            this.add(_.map(source.children('div'), _.bind(function(e) {
+            if (sourceContent.find('#load-previous')) {
+                if (destContent) {
+                    destContent.find('#load-previous').replaceWith(
+                        sourceContent.find('#load-previous')
+                    );
+                }
+            }
+            if (sourceContent.find('#load-more')) {
+                if (destContent) {
+                    destContent.find('#load-more').replaceWith(
+                        sourceContent.find('#load-more')
+                    );
+                }
+            }
+            
+            this.add(_.map(sourceTranscript.children('div'), _.bind(function(e) {
                 var logLine = new Artemis.LogLine.fromElement(e);
+                if (destTranscript) {
+                    if (logLine.id < this.first().id) {
+                        $('#transcript').prepend(e);
+                    }
+                    else {
+                        $('#transcript').append(e);
+                    }
+                }
                 return logLine;
             }, this)));
         }
@@ -111,52 +146,88 @@
         initialize: function(options) {
             this.route('/page/', 'page', this.page);
             this.route('/page/('+tspatt+')/', 'page', this.page);
+            this.route('/('+tspatt+')/', 'logLine', this.logLine);
+            this.route('/('+tspatt+')/('+tspatt+')/', 'logLine', this.logLine);
+            
+            // After a route is triggered, save it as the previous route
+            this.previous = null;
+            this.bind('all', function(eventName) {
+                var bits = eventName.split(':');
+                if (bits[0] == 'route') {
+                    this.previous = bits[1];
+                }
+            });
+        },
+        
+        page: function(start) {
+            this.checkContentClasses(['transcript']);
+            this.transition('page', start);
+        },
+        
+        logLine: function(start, end) {
+            this.checkContentClasses(['transcript']);
+            this.transition('logLine', start, end);
         },
 
-        page: function(start, end) {
-            if (!$('#content').hasClass('transcript')) {
-                Backbone.history.navigateToPath();
-                return;
-            }
-            if (!this.logLineCollection) {
-                this.logLineCollection = new Artemis.LogLineCollection();
-                this.logLineCollection.updateFromElement();
-            }
-            else {
-                $.getJSON(Backbone.history.getPath(), _.bind(function(data) { 
-                    $('title').text(data.title);
-                    // If there is a crest, load the entire page
-                    if ($(data.crest).children().length) {
-                        $('#content').children().html(data.content);
-                        $('#crest').replaceWith(data.crest);
-                    }
-                    else {
-                        content = $(data.content);
-                        var transcript = content.filter('#transcript');
-                        this.logLineCollection.updateFromElement(transcript);
-                        if (parseInt(transcript.children('div').attr('id').split('-')[2], 10) < this.logLineCollection.first().id) {
-                            $('#transcript').prepend(transcript.children());
-                        }
-                        else {
-                            $('#transcript').append(transcript.children());
-                        }
-                        if ($('#load-previous')) {
-                            $('#load-previous').replaceWith(content.find('#load-previous'));
-                        }
-                        if ($('#load-more')) {
-                            $('#load-more').replaceWith(content.find('#load-more'));
-                        }
-                    }
-
-                }, this));
+        transition: function(next) {
+            var prev = Artemis.transitions[this.previous]
+            if (prev && prev[next]) {
+                prev[next].apply(this, Array.prototype.slice.call(arguments, 1));
             }
         },
         
+        checkContentClasses: function(classes) {
+            if (!_.all(classes, function(c) { return $('#content').hasClass(c) })) {
+                Backbone.history.navigateToPath();
+                return;
+            }
+        },
 
         _routeToRegExp: function(route) {
             return new RegExp('^' + route + '$');
         }
     });
+
+    Artemis.transitions = {
+        null: {
+            'page': function() {
+                this.logLineCollection = new Artemis.LogLineCollection();
+                this.logLineCollection.updateFromContent();
+            }
+        },
+
+        'page': {
+            'page': function() {
+                $.getJSON(Backbone.history.getPath()+'?json', _.bind(function(data) { 
+                    $('title').text(data.title);
+                    // If there is a crest, load the entire page
+                    // TODO: do this if this page is not next/prev
+                    if ($(data.crest).children().length) {
+                        $('#content').children().html(data.content);
+                        $('#crest').replaceWith(data.crest);
+                        $(document).scrollTop(0);
+                    }
+                    else {
+                        content = $(data.content);
+                        this.logLineCollection.updateFromContent(content);
+                    }
+
+                }, this));
+            },
+            'logLine': function() {
+                var content = $('#content');
+                content.addClass('with-highlight');
+            }
+        },
+
+
+        'logLine': {
+            'page': function() {
+                var content = $('#content');
+                content.removeClass('with-highlight');
+            }
+        }
+    };
 
     $(function() {
         Backbone.history = new Artemis.History();
