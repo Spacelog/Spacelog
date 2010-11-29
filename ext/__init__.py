5170dd49b1222ea767d38adf58fb267326660d84
James Aylett <james@devfort.com>
2010-11-29 14:40:55 +0000

    Remove Django 1.3 alpha 1 from ext/ since we'll grab Django trunk from SVN until 1.3 is released.

--- ext/__init__.py
+++ ext/__init__.py
@@ -4,5 +4,4 @@ import sys
 our_dir = os.path.dirname(__file__)
 
 sys.path.insert(0, os.path.join(our_dir, "redis-py"))
-sys.path.insert(0, os.path.join(our_dir, "Django-1.3-alpha-1"))
 sys.path.insert(0, os.path.join(our_dir, "xappy-0.5-sja-1"))
