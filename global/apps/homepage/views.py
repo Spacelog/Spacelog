f8ab3afa13f17850af7b5ffcfc836d377557218a
Matthew Somerville <matthew@dracos.co.uk>
2010-12-01 20:16:45 +0000

    Make badge colour, no longer coming soon.

--- global/apps/homepage/views.py
+++ global/apps/homepage/views.py
@@ -21,12 +21,6 @@ def homepage(request):
             'title': 'Apollo 8',
             'description': 'The first human space flight to leave Earth orbit.'
         },
-        {
-            'name': 'a11',
-            'coming_soon': True,
-            'title': 'Apollo 11',
-            'description': 'The first manned mission to land on the moon.'
-        }
     ]
     return render_to_response(
         'homepage/homepage.html',
