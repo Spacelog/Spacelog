43f69cfab1affeb19decb22ceb34e5f4ddbe7e19
Steve Marshall <steve@nascentguruism.com>
2011-01-29 14:24:34 +0000

    Work around iOS `position: fixed;` limitations.
    iOS treats `position: fixed;` as absolute after first-render, so use JS to listen for start and end of scrolling. On start, hide the phases nav, on end, redisplay it. Closes #41.

--- website/static/js/global.js
+++ website/static/js/global.js
@@ -1,3 +1,51 @@
+/* scrollstart and scrollstop cribbed directly from jQuery Mobile
+ *
+ * Copyright 2010, jQuery Project
+ * Dual licensed under the MIT or GPL Version 2 licenses.
+ * http://jquery.org/license
+ */
+var scrollEvent = "touchmove scroll";
+$.event.special.scrollstart = {
+    enabled: true,
+    
+    setup: function() {
+        var thisObject = this,
+            $this = $( thisObject ),
+            scrolling,
+            timer;
+        
+        function trigger( event, state ) {
+            scrolling = state;
+            var originalType = event.type;
+            event.type = scrolling ? "scrollstart" : "scrollstop";
+            $.event.handle.call( thisObject, event );
+            event.type = originalType;
+        }
+        
+        // iPhone triggers scroll after a small delay; use touchmove instead
+        $this.bind( scrollEvent, function( event ) {
+            if ( !$.event.special.scrollstart.enabled ) {
+                return;
+            }
+            
+            if ( !scrolling ) {
+                trigger( event, true );
+            }
+            
+            clearTimeout( timer );
+            timer = setTimeout(function() {
+                trigger( event, false );
+            }, 50 );
+        });
+    }
+};
+$.event.special.scrollstop = {
+    setup: function() {
+        $( this ).bind( "scrollStart", $.noop );
+    }
+};
+// End jQuery cribbed code
+
 (function() {
     var Artemis = {
         animationTime: 250,
@@ -51,8 +99,62 @@
         $('input[placeholder], textarea[placeholder]').placeholder();
 
         Artemis.homepageQuoteView = new Artemis.HomepageQuoteView();
+        
+        // HACK: Replace position: fixed with update-on-scroll for footer
+        var userAgent    = navigator.userAgent.toLowerCase();
+        var phasesFooter = $( '#phases' );
+        if ( -1 < userAgent.indexOf( 'webkit' ) && phasesFooter.length ) {
+            var mobileUAs = [
+                // iOS devices
+                'ipad', 'ipod', 'iphone',
+                // Android
+                'android',
+            ];
             
-
+            // Cycle through each mobile UA we care about and see
+            // if this is one of them
+            var isMobile = false;
+            for (var i = mobileUAs.length - 1; i >= 0; i--){
+                var mobileUA = mobileUAs[i];
+                
+                if ( -1 < userAgent.indexOf( mobileUA ) ) {
+                    isMobile = true;
+                    break;
+                }
+            };
+            
+            // If we have a mobile UA, change the footer to update
+            if ( isMobile ) {
+                var phasesShower = null;
+                
+                // Hide the footer when scrolling
+                $( document ).bind( 'scrollstart', function () {
+                    phasesFooter.hide();
+                    clearTimeout( phasesShower );
+                } );
+                
+                // Re-show the footer when scrolling stops
+                $( document ).bind( 'scrollstop', function () {
+                    phasesFooter.css({
+                        'top': $( window ).scrollTop() + window.innerHeight - phasesFooter.height(),
+                    });
+                    phasesShower = setTimeout( function () {
+                        phasesFooter.fadeIn();
+                    }, 500 );
+                } );
+                
+                // alert( $( window ).height() + ":" + window.innerHeight + ":" + document.body.offsetHeight );
+                // Forcibly reset the footer position on load
+                phasesFooter.hide();
+                setTimeout( function () {
+                    phasesFooter.css({
+                        'position': 'absolute',
+                        'top': $( window ).scrollTop() + window.innerHeight - phasesFooter.height(),
+                    });
+                    phasesFooter.show();
+                }, 500 );
+            }
+        }
     });
 
     window.Artemis = Artemis;
