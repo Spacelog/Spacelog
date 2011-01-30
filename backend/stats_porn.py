2ae621f4bfdd4b544ba1dfc6504c0b37948657a3
George Brocklehurst <george.brocklehurst@gmail.com>
2011-01-30 13:48:41 +0000

    Statsporn should use the current redis DB.

--- backend/stats_porn.py
+++ backend/stats_porn.py
@@ -129,5 +129,9 @@ class StatsPornGenerator(object):
 
 if __name__ == "__main__":
     redis_conn = redis.Redis()
+    current_db = int(redis_conn.get("live_database") or 0)
+    print "Building visualisations from database %d" % current_db
+    redis_conn.select(current_db)
+
     generator = StatsPornGenerator(redis_conn)
     generator.build_all_missions()
