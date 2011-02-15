fe4fcbaf54bb63015f103ede1aba3d6cce38b380
George Brocklehurst <george.brocklehurst@gmail.com>
2011-02-15 20:52:08 +0000

    Home page port should match make task's port.

--- website/configs/development/settings.py
+++ website/configs/development/settings.py
@@ -2,7 +2,7 @@ from configs.settings import *
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
-PROJECT_HOME = "http://artemis:3000/"
+PROJECT_HOME = "http://artemis:8001/"
 
 try:
     from local_settings import *
