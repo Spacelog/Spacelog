da0eacf9783e8ea8679f90b68d5f2b017ed1eb85
James Aylett <james@tartarus.org>
2011-03-13 15:41:38 +0000

    Draft reading list. People will no doubt hate where I've put it, although it can be removed from individual global about pages very easily. (Or it could become its own page without much fuss.)
    Sort out vertical pacing for about pages.
    Draft about copy, needs details checking & adding.

--- global/apps/homepage/context.py
+++ global/apps/homepage/context.py
@@ -5,4 +5,69 @@ def static(request):
         "STATIC_URL": settings.STATIC_URL,
         "FIXED_STATIC_URL": settings.FIXED_STATIC_URL,
         "MISSIONS_STATIC_URL": settings.MISSIONS_STATIC_URL,
+        "READING_LISTS": [
+            [
+                "By NASA astronauts and Soviet cosmonauts",
+                [
+                    [
+                        "Deke!",
+                        "Deke Slayton",
+                        [
+                            [ "http://amazon.com/", "Amazon US", ],
+                            [ "http://amazon.co.uk/", "Amazon UK", ],
+                        ],
+                    ],
+                    [
+                        "We Seven",
+                        "The Mercury Astronauts",
+                        [
+                            [ "http://amazon.com/", "Amazon US", ],
+                            [ "http://amazon.co.uk/", "Amazon UK", ],
+                        ],
+                    ],
+                    [
+                        "We Have Capture",
+                        "Tom Stafford",
+                        [
+                            [ "http://amazon.com/", "Amazon US", ],
+                            [ "http://amazon.co.uk/", "Amazon UK", ],
+                        ],
+                    ],
+                ],
+            ],
+            [
+                "By other principals",
+                [
+                    [
+                        "Flight",
+                        "Chris Kraft",
+                        [
+                            [ "http://amazon.com/", "Amazon US", ],
+                            [ "http://amazon.co.uk/", "Amazon UK", ],
+                        ],
+                    ],
+                    [
+                        "Mars Project",
+                        "Werner von Braun",
+                        [
+                            [ "http://amazon.com/", "Amazon US", ],
+                            [ "http://amazon.co.uk/", "Amazon UK", ],
+                        ],
+                    ],
+                ],
+            ],
+            [
+                "Other books",
+                [
+                    [
+                        "Full Moon",
+                        "",
+                        [
+                            [ "http://amazon.com/", "Amazon US", ],
+                            [ "http://amazon.co.uk/", "Amazon UK", ],
+                        ],
+                    ],
+                ],
+            ],
+        ],
     }
