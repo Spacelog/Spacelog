514d66c190415c3c78bb59f314608b7ddd0db9b3
George Brocklehurst <george@georgebrock.com>
2015-04-08 00:42:57 -0400

    Add sass to Vagrant.

--- chef/spacelog/recipes/default.rb
+++ chef/spacelog/recipes/default.rb
@@ -1,6 +1,7 @@
 execute "apt-get update"
 
 %w{python python-setuptools python-pip redis-server python-redis
-    python-virtualenv imagemagick subversion git-core python-xapian}.each { |p|
+    python-virtualenv imagemagick subversion git-core python-xapian
+    ruby-sass}.each { |p|
   package p
 }
